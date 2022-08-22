bb0d641be4a2ac08ae790496b2567eb6b8cf78a6
Danny Rodriguez <danny007111@hotmail.com>
2022-08-22 15:56:48 -0500

    Completed assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,7 +8,8 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    # bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(params.permit(:name, :species))
     render json: bird, status: :created
   end
 
@@ -22,4 +23,19 @@ class BirdsController < ApplicationController
     end
   end
 
+#   You can disable the wrap parameters feature in an individual controller:
+
+# class BirdsController < ApplicationController
+#   wrap_parameters format: []
+# end
+# You can also disable it for all controllers if you like, by going into the config/initializers/wrap_parameters.rb file and updating it like so:
+
+# ActiveSupport.on_load(:action_controller) do
+#   wrap_parameters format: []
+# end
+
+  private 
+  def bird_params
+    params.permit(:name, :species)
+  end
 end
